a85dc297a41c01f5bfb8d348a80ddcb0f0a85909
wilfred11 <wilfredvancasteren@hotmail.com>
2025-06-20 17:21:54 +0200

    changes

--- exams.py
+++ exams.py
@@ -4,6 +4,8 @@ from causallib.estimation import IPW
 from causallib.evaluation import evaluate
 from matplotlib import pyplot as plt
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import KFold, cross_val_score, StratifiedKFold
+
 
 def get_dummies(data):
     lunch_dummy = pd.get_dummies(data['lunch'])
@@ -37,18 +39,87 @@ def get_exams_data():
     return exam_data
 
 def ipw(data):
-    learner = LogisticRegression()
-    #learner = RandomForestClassifier(n_estimators=1000)
-    # learner = KNeighborsClassifier(n_neighbors=3)
-    # clip_max=0.6798,clip_min=1-0.6798, verbose=True, use_stabilized=False
-    ipw = IPW(learner)
+    learner = LogisticRegression(max_iter=1000, random_state=21)
+    ipw = IPW(learner, clip_min=0.05, clip_max=0.95)
     ipw.fit(data[x_columns()], data['test preparation course'])
-    results = evaluate(ipw, data[x_columns()], data['test preparation course'], data['math score'], cv="auto", plots=True )
-    # print(ipw.compute_weights(data[['unemp_rate', 'technician_pc']],data['factory']).head())
-    # print(ipw.compute_weights(data[['unemp_rate', 'technician_pc']],data['factory']))
-    fig, ax = plt.subplots(1, 1, figsize=(6, 6))
-    results.plot_covariate_balance(kind="love", ax=ax, thresh=0.1)
-    plt.show()
+    k_fold = StratifiedKFold(n_splits=2, random_state=21, shuffle=True)
+    k_fold_splits =  k_fold.split(data[x_columns()], data['test preparation course'], groups=None)
+    results = evaluate(ipw, data[x_columns()], data['test preparation course'], data['math score'], cv=k_fold_splits)
+
+    print(ipw.compute_weights(data[x_columns()], data['test preparation course']).head())
+
+    outcomes = ipw.estimate_population_outcome(data[x_columns()], data['test preparation course'], data['math score'])
+    print(outcomes)
+    effect = ipw.estimate_effect(outcomes[1], outcomes[0])
+    print(effect)
+
+    print(ipw.compute_propensity_matrix(data[x_columns()], data['test preparation course']))
+    hist=ipw.compute_propensity_matrix(data[x_columns()], data['test preparation course'])
+    hist['test preparation course']= data['test preparation course']
+    hist_treated = hist[hist['test preparation course']==True]
+    hist_untreated = hist[hist['test preparation course'] == False]
+    plt.hist(hist_treated[True],
+             alpha=0.5,
+             label='test preparation course', bins=10)
+
+    plt.hist(hist_untreated[True],
+             alpha=0.5,
+             label='no test preparation course(CG)', bins=10)
+
+    plt.legend(loc='upper right')
+    plt.title('Propensity scores histogram')
+    plt.legend()
+    #plt.show()
+    plt.savefig("out/ipw/prop_histo")
+    plt.clf()
+
+
+    fig, [a0, a1] = plt.subplots(1, 2, figsize=(12, 6))
+    results.plot_covariate_balance(kind="love", phase="train", ax=a0, thresh=0.1)
+    results.plot_covariate_balance(kind="love", phase="valid", ax=a1, thresh=0.1)
+    for ax, suffix in [(a0, "Train"), (a1, "Validation")]:
+        ax.set_title(ax.get_title() + ": " + suffix)
+    plt.tight_layout()
+    plt.savefig("out/ipw/love")
+    plt.clf()
+
+    fig, [a0, a1] = plt.subplots(1, 2, figsize=(12, 6))
+    results.plot_covariate_balance(kind="slope", phase="train", ax=a0, thresh=0.1)
+    results.plot_covariate_balance(kind="slope", phase="valid", ax=a1, thresh=0.1)
+    for ax, suffix in [(a0, "Train"), (a1, "Validation")]:
+        ax.set_title(ax.get_title() + ": " + suffix)
+    plt.tight_layout()
+    plt.savefig("out/ipw/slope")
+    plt.clf()
+
+    fig, [a0, a1] = plt.subplots(1, 2, figsize=(12, 6))
+    results.plot_weight_distribution(reflect=True, ax=a0)
+    results.plot_weight_distribution(reflect=True, phase="valid", ax=a1)
+    for ax, suffix in [(a0, "Train"), (a1, "Validation")]:
+        ax.set_title(ax.get_title() + ": " + suffix)
+    plt.tight_layout()
+    plt.savefig("out/ipw/weight_distri")
+    plt.clf()
+
+    fig, [a0, a1] = plt.subplots(1, 2, figsize=(12, 6))
+    results.plot_roc_curve(ax=a0)
+    results.plot_roc_curve(phase="valid", ax=a1)
+    for ax, suffix in [(a0, "Train"), (a1, "Validation")]:
+        ax.set_title(ax.get_title() + ": " + suffix)
+    plt.tight_layout()
+    plt.savefig("out/ipw/roc")
+    plt.clf()
+
+    fig, [a0, a1] = plt.subplots(1, 2, figsize=(12, 6))
+    results.plot_pr_curve(ax=a0)
+    results.plot_pr_curve(phase="valid", ax=a1)
+    for ax, suffix in [(a0, "Train"), (a1, "Validation")]:
+        ax.set_title(ax.get_title() + ": " + suffix)
+    plt.tight_layout()
+    plt.savefig("out/ipw/pr")
+    plt.clf()
+
+    #plt.show()
 
 def x_columns():
     return ['standard', 'group B', 'group C', 'group D', 'group E',
